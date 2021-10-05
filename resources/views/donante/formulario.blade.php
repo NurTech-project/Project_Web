a070e6c2bfe1f34fdc1dba075e299cf1eadd3fc6
CarolinaChamorro <carolinalisicsb@gmail.com>
2021-10-04 22:07:51 -0500

    donante

---
+++ resources/views/donante/formulario.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Formulario de donación') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <!-- Formulario -->
+    </div>
+</x-app-layout>
