bc05b1e0eea8ba88108d49c8b954816cf573ad7f
Angelo2001Diaz <psychorealm641@gmail.com>
2021-10-15 18:20:32 -0500

    vistaHome

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -12,29 +12,27 @@
 <body>
 
 
-
+  <br><br>
     <div class="lg-flex bg-cover bg-center  h-auto text-black py-36 px-10 object-fill" style="background-image: url(http://nur.yavirac.edu.ec/images/slide_4.png)">
 
       <div class="md:w-1/2">
        <p class="text-8xl font-serif text-center">NUR-TECH</p><br>
-       <p class="text-2xl font-serif mb-10 leading-none text-center">DONANDO TU COMPUTADORA<br>
+       <p class="text-2xl font-serif mb-10 leading-none text-center">DONANDO TU <b>COMPUTADORA</b> <br>
         AYUDARAS A UN ESTUDIANTE<br>
         MAS, A CONTINUAR <br>
         CON SUS ESTUDIOS <br>
        </p>
        <br><br>
-
-       <a href="#" class="content-center bg-purple-500 py-4 px-64 font-serif uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">Donar Ahora</a>
-       </div>  
+        <b>
+          <a href="{{ url('/register') }}" class=" content-center bg-purple-500 py-4 px-64 font-serif uppercase text-sm rounded hover:bg-gray-200 hover:text-gray-800">Donar Ahora</a>  
+        </b>       
+      </div>  
     </div>
     
-        <!--======= HOW CAN YOU HELP =========-->
-        <section class="how-can-help what-we">
-          <div class="container">
+        <section class="how-can-help what-we container">
             <ul class="lg:flex space-x-4">
               
-              <!--======= HOW CAN YOU HELP =========-->
-              <li class="p-10 w-75 font-serif"> <span class="text-6xl "><b>¿QUÉ</b> 
+              <li class="p-32 w-75 font-serif"> <span class="text-6xl "><b>¿QUÉ</b> 
                 <br>
                 <small>ES NUR TECH?</small></span>
                 <hr>
@@ -54,39 +52,73 @@
                 
               </li>
               
-              <!--======= OUR PROGRAM =========-->
                 <li> <img src="imagenes/dona.jpg" class="lg:flex  py-56 max-w-lg center " ></li>
 
             </ul>
-          </div>
         </section>
       
     
     
-        <!--======= CALL OUT =========-->
         <section class="h-50">
             <div class="font-serif text-center text-3xl py-12 bg-cover bg-center  h-auto text-black px-10 object-fill" style="background-image: url(https://www.ppt-backgrounds.net/thumbs/yellow-geometric-low-poly-graphic-presentation-backgrounds.jpg)" >
               <h2><strong>Buscamos</strong></h2><br>
               <h2>Computadores usados <b>o</b> partes y piezas </h2>
           </div>
+
         </section>
-        <br><br>
-    <!--======= PROCESO =========-->
-    <div class="p-10 font-serif"> <span class="text-6xl">¿Cómo funciona?</span>
-      <hr>
+    
+        
+    <section class=" how-can-help">
+      <div class="container font-serif">
+        <ul class="lg:flex">
+          <li class="p-32 "> <span class="text-6xl">¿Cómo <br><small>puedo ayudar?</small></span>
+            <hr>
+            <br>
+            <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de 
+              relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) 
+              desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>           
+               <br>
+              <button class="bg-transparent hover:bg-purple-700 text-black py-2 px-4 border hover:text-white border-purple-700 hover:border-transparent rounded">Quiero Participar</button>           
+           
+                 
+          <li class=" p-20">
+            <ul class="help-fea">
+              <br>
+              <li ><img src="imagenes\a.png" class="h-10 "> Dona un computador</li><br>
+              <li ><img src="imagenes\b.png" class="h-10 "> Dona partes y piezas</li><br>
+              <li ><img src="imagenes\c.png" class="h-10 "> Promueve el proyecto en tus redes</li> <br>         
+              <li ><img src="imagenes\d.png" class="h-10 "> Hazte voluntario</li>              
+            </ul>
+          </li> 
+          
+        </ul>
+      </div>
+    </section>
+
+
+    
+
+    <section class="h-50 font-serif">
+      <div class=" bg-purple-700 text-center text-3xl text-white py-12 ">
+        <h2>La educación es uno de los factores que más influye en el avance <br>
+          y progreso de personas y sociedades</h2>
+        
+        
     </div>
+  </section>
 
 
-    <ul class="lg:flex  p-10 font-serif">       
-        <!--======= RECOLECTAR =========-->
-        <li> <img src="imagenes\recolectar.png" class="h-20 w-70" >
+  <div class="p-16 font-serif text-center"> <span class="text-6xl">¿Cómo funciona?</span>
+    <hr>
+  </div>
+    <ul class="lg:flex  p-10 font-serif text-aling">       
+        <li> <img src="imagenes\b.png" class="h-20 w-70" >
           <b><h4>Recolectar</h4></b>
           <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de 
             relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) 
             desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
           
         
-        <!--======= ACONDCIONAR =========-->
         <li> <img src="imagenes\acondicionar.jpg" class="h-20 w-70" >
           <b><h4>Acondicionar</h4></b>
           <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de 
@@ -94,27 +126,38 @@
             desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
           
         
-        <!--======= ENTREGAR =========-->
         
         <li> <img src="imagenes\entregar.jpg" class="h-20 w-70" >
           <b><h4>Entregar</h4></b>
           <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de 
             relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) 
             desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
-          <a href="#"></a>Dona ahora</a> </li>
+         </li>
       </ul>
   
       <br><br>  
 
-          <section class="h-50">
+      
+
+          <section class="h-50 font-serif">
             <div class=" text-center text-3xl py-12 bg-cover bg-center  h-auto text-black px-10 object-fill" style="background-image: url(https://www.ppt-backgrounds.net/thumbs/yellow-geometric-low-poly-graphic-presentation-backgrounds.jpg)" >
-              <h2>La educación es uno de los factores que más influye en el avance y progreso de personas y sociedades</h2><br>
+              <h1>10</h1>
+              <h2>Estamos muy orgullosos de nuestros Amables<strong>Dontantes</strong></h2>
           </div>
         </section>
           
           <br><br>
 
-          
+          <div class="container">
+            <center>
+              <div>
+                <img class="h-auto" src="/imagenes/donaciones.png" alt="" > 
+              </div>
+            </center>
+          </div>
+          <br>
+          <hr>
+          <br>
         <ul class="lg:flex p-10 font-serif">      
 
           <li class="p-10 w-auto "> <span class="text-4xl "><b>¡QUIERO</b> 
@@ -134,14 +177,14 @@
           
           <li>
             <br><br>
-            <button class="text-black bg-yellow-400 hover:bg-black  hover:text-white font-semibold py-2 px-24 rounded " >DONAR UN COMPUTADOR</button>
+            <button class="bg-transparent hover:bg-purple-700 text-black border-2 hover:text-white border-purple-700 hover:border-transparent font-serif py-2 px-24 rounded "  > <a href="{{ url('/register') }}"> DONAR UN COMPUTADOR</a></button>
           </li>
 
           
 
         </ul>
       
-         
+       
 
     
     </body>
