fc0ea31290341acb348fa49e4e865429fb80e878
Angelo2001Diaz <psychorealm641@gmail.com>
2021-10-19 15:15:17 -0500

    cambiosDistribuidor

--- resources/views/distribuidor/create-pieza.blade.php
+++ resources/views/distribuidor/create-pieza.blade.php
@@ -9,7 +9,7 @@
     <div class="py-12 px-60">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-16 bg-white border-b border-gray-200">
+                <div class="p-16 bg-white border-b border-gray-200 text-center">
                     <h2 class=" mb-4 font-black text-3xl  text-center ">Detalle Pieza</h2>
                     @foreach ($piezaGet as $pieza)
                     <br><hr>
